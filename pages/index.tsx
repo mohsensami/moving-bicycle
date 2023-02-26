ee0d3fb941f527a85bd4fa00e4013be4f57f50e0
MohsenSami <mohsensami88@gmail.com>
2023-02-26 21:10:54 +0330

    move bicycle by css animation keyframes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from "@/styles/Home.module.css"
+import Bicycle from '@/components/img/bicycle.png'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="relative bg-[url('../components/img/bg.jpg')] bg-cover h-[800px] w-[800px] border-2 border-gray-200 mx-auto mt-8">
+      <Image src={Bicycle.src} alt="Bicycle" width={150} height={118} className={'absolute left-0 top-[209px] ' + styles.move}/>
+    </div>
   )
 }
